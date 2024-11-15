4c344bdf77b920321df9ebfc2966389057a87a73
samuel2935 <samuelblessed38@gmail.com>
2024-11-15 15:59:20 -0800

    debugged

--- src/hooks/WalletProvider.tsx
+++ src/hooks/WalletProvider.tsx
@@ -1,37 +1,39 @@
-import {
-    PropsWithChildren,
-    createContext,
-    useCallback,
-    useEffect,
-    useState,
-  } from "react"
-  
+import { PropsWithChildren, createContext, useCallback, useEffect, useState } from 'react'
 
 
+type SelectedAccountByWallet = Record<string, string | null>
 
-  declare global {
-    interface WindowEventMap {
-      "eip6963:announceProvider": CustomEvent
-    }
+interface WalletProviderContext {
+  wallets: Record<string, EIP6963ProviderDetail>         // Record of wallets by UUID
+  selectedWallet: EIP6963ProviderDetail | null           // Currently selected wallet
+  selectedAccount: string | null                         // Account address of selected wallet
+  errorMessage: string | null                            // Error message
+  connectWallet: (walletUuid: string) => Promise<void>   // Function to trigger wallet connection
+  disconnectWallet: () => void                           // Function to trigger wallet disconnection
+  clearError: () => void                                 // Function to clear error message
+}
+
+declare global{
+  interface WindowEventMap {
+    'eip6963:announceProvider': CustomEvent
   }
+}
+
+export const WalletProviderContext = createContext<WalletProviderContext | undefined>(undefined);
 
 
-// The WalletProvider component wraps all other components in the dapp, providing them with the
-// necessary data and functions related to wallets.
-export const WalletProviderContext = createContext<WalletProviderContext>(null)
 export const WalletProvider: React.FC<PropsWithChildren> = ({ children }) => {
-
   const [wallets, setWallets] = useState<Record<string, EIP6963ProviderDetail>>({})
   const [selectedWalletRdns, setSelectedWalletRdns] = useState<string | null>(null)
   const [selectedAccountByWalletRdns, setSelectedAccountByWalletRdns] = useState<SelectedAccountByWallet>({})
 
-  const [errorMessage, setErrorMessage] = useState("")
-  const clearError = () => setErrorMessage("")
+  const [errorMessage, setErrorMessage] = useState('')
+  const clearError = () => setErrorMessage('')
   const setError = (error: string) => setErrorMessage(error)
 
   useEffect(() => {
-    const savedSelectedWalletRdns = localStorage.getItem("selectedWalletRdns")
-    const savedSelectedAccountByWalletRdns = localStorage.getItem("selectedAccountByWalletRdns")
+    const savedSelectedWalletRdns = localStorage.getItem('selectedWalletRdns')
+    const savedSelectedAccountByWalletRdns = localStorage.getItem('selectedAccountByWalletRdns')
 
     if (savedSelectedAccountByWalletRdns) {
       setSelectedAccountByWalletRdns(JSON.parse(savedSelectedAccountByWalletRdns))
@@ -48,106 +50,73 @@ export const WalletProvider: React.FC<PropsWithChildren> = ({ children }) => {
       }
     }
 
-    window.addEventListener("eip6963:announceProvider", onAnnouncement)
-    window.dispatchEvent(new Event("eip6963:requestProvider"))
-
-    return () => window.removeEventListener("eip6963:announceProvider", onAnnouncement)
+    window.addEventListener('eip6963:announceProvider', onAnnouncement)
+    window.dispatchEvent(new Event('eip6963:requestProvider'))
+    
+    return () => window.removeEventListener('eip6963:announceProvider', onAnnouncement)
   }, [])
 
+  const connectWallet = useCallback(async (walletRdns: string) => {
+    try {
+      const wallet = wallets[walletRdns]
+      const accounts = await wallet.provider.request({method:'eth_requestAccounts'}) as string[]
 
+      if(accounts?.[0]) {
+        setSelectedWalletRdns(wallet.info.rdns)
+        setSelectedAccountByWalletRdns((currentAccounts) => ({
+          ...currentAccounts,
+          [wallet.info.rdns]: accounts[0],
+        }))
 
-  const connectWallet = useCallback(
-    async (walletRdns: string) => {
-      try {
-        const wallet = wallets[walletRdns]
-        const accounts = (await wallet.provider.request({
-          method: "eth_requestAccounts",
-        })) as string[]
-  
-        if (accounts?.[0]) {
-          setSelectedWalletRdns(wallet.info.rdns)
-          setSelectedAccountByWalletRdns((currentAccounts) => ({
-            ...currentAccounts,
-            [wallet.info.rdns]: accounts[0],
-          }))
-  
-          localStorage.setItem("selectedWalletRdns", wallet.info.rdns)
-          localStorage.setItem(
-            "selectedAccountByWalletRdns",
-            JSON.stringify({
-              ...selectedAccountByWalletRdns,
-              [wallet.info.rdns]: accounts[0],
-            })
-          )
-        }
-      } catch (error) {
-        console.error("Failed to connect to provider:", error)
-        const walletError: WalletError = error as WalletError
-        setError(
-          `Code: ${walletError.code} \nError Message: ${walletError.message}`
-        )
+        localStorage.setItem('selectedWalletRdns', wallet.info.rdns)
+        localStorage.setItem('selectedAccountByWalletRdns', JSON.stringify({
+          ...selectedAccountByWalletRdns,
+          [wallet.info.rdns]: accounts[0],
+        }))
       }
-    },
-    [wallets, selectedAccountByWalletRdns]
-  )
+    } catch (error) {
+      console.error('Failed to connect to provider:', error)
+      const walletError: WalletError = error as WalletError
+      setError(`Code: ${walletError.code} \nError Message: ${walletError.message}`)
+    }
+  }, [wallets, selectedAccountByWalletRdns])
 
 
   const disconnectWallet = useCallback(async () => {
-  if (selectedWalletRdns) {
-    setSelectedAccountByWalletRdns((currentAccounts) => ({
-      ...currentAccounts,
-      [selectedWalletRdns]: null,
-    }))
+    if (selectedWalletRdns) {
+      setSelectedAccountByWalletRdns((currentAccounts) => ({
+        ...currentAccounts,
+        [selectedWalletRdns]: null,
+      }))
 
-    const wallet = wallets[selectedWalletRdns]
-    setSelectedWalletRdns(null)
-    localStorage.removeItem("selectedWalletRdns")
+      const wallet = wallets[selectedWalletRdns];
+      setSelectedWalletRdns(null)
+      localStorage.removeItem('selectedWalletRdns')
 
-    try {
-      await wallet.provider.request({
-        method: "wallet_revokePermissions",
-        params: [{ eth_accounts: {} }],
-      })
-    } catch (error) {
-      console.error("Failed to revoke permissions:", error)
+      try {
+        await wallet.provider.request({
+          method: 'wallet_revokePermissions',
+          params: [{ 'eth_accounts': {} }]
+        });
+      } catch (error) {
+        console.error('Failed to revoke permissions:', error);
+      }
     }
-  }
-}, [selectedWalletRdns, wallets])
+  }, [selectedWalletRdns, wallets])
 
-
-const contextValue: WalletProviderContext = {
+  const contextValue: WalletProviderContext = {
     wallets,
-    selectedWallet:
-      selectedWalletRdns === null ? null : wallets[selectedWalletRdns],
-    selectedAccount:
-      selectedWalletRdns === null
-        ? null
-        : selectedAccountByWalletRdns[selectedWalletRdns],
+    selectedWallet: selectedWalletRdns === null ? null : wallets[selectedWalletRdns],
+    selectedAccount: selectedWalletRdns === null ? null : selectedAccountByWalletRdns[selectedWalletRdns],
     errorMessage,
     connectWallet,
     disconnectWallet,
     clearError,
   }
-  
+
   return (
     <WalletProviderContext.Provider value={contextValue}>
       {children}
     </WalletProviderContext.Provider>
   )
-  // Type alias for a record where the keys are wallet identifiers and the values are account
-  // addresses or null.
-  type SelectedAccountByWallet = Record<string, string | null>
-  
-  // Context interface for the EIP-6963 provider.
-  interface WalletProviderContext {
-    wallets: Record<string, EIP6963ProviderDetail> // A list of wallets.
-    selectedWallet: EIP6963ProviderDetail | null // The selected wallet.
-    selectedAccount: string | null // The selected account address.
-    errorMessage: string | null // An error message.
-    connectWallet: (walletUuid: string) => Promise<void> // Function to connect wallets.
-    disconnectWallet: () => void // Function to disconnect wallets.
-    clearError: () => void
-  }
-
-
 }
--- src/hooks/useWalletProvider.tsx
+++ src/hooks/useWalletProvider.tsx
@@ -1,4 +1,17 @@
 import { useContext } from "react"
 import { WalletProviderContext } from "./WalletProvider"
 
-export const useWalletProvider = () => useContext(WalletProviderContext)
+
+// export const useWalletProvider = () => useContext(WalletProviderContext)
+export const useWalletProvider = () => {
+    const context = useContext(WalletProviderContext);
+  
+    if (!context) {
+      throw new Error("useWalletContext must be used within a WalletProvider");
+    }
+  
+    return context;
+  };
+ 
+
+
